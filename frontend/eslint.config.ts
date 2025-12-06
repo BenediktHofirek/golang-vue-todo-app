9b5cd26f7c8b28a1cb2143e4d015f5cc85817a0e
Benedikt Hofirek <benedikthofirek@gmail.com>
2025-12-06 15:31:35 +0100

    configure tailwindcss class formatting

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -3,6 +3,8 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+import eslintParserVue from "vue-eslint-parser";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -21,4 +23,32 @@ export default defineConfigWithVueTs(
   vueTsConfigs.recommended,
   ...pluginOxlint.configs['flat/recommended'],
   skipFormatting,
+
+  // eslint-plugin-better-tailwindcss
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: eslintParserVue
+    }
+  },
+  {
+    plugins: {
+      "better-tailwindcss": eslintPluginBetterTailwindcss
+    },
+    rules: {
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
+
+      // or configure rules individually
+      "better-tailwindcss/enforce-consistent-line-wrapping": ["warn", { printWidth: 100 }]
+    },
+    settings: {
+      "better-tailwindcss": {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: "src/assets/style.css",
+      }
+    }
+  }
 )
--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { onMounted, shallowRef } from 'vue';
+import { onMounted, shallowRef } from 'vue'
 import AddTask from '@material-symbols/svg-600/outlined/add_task.svg'
-import Todo from './Todo.vue';
+import Todo from './Todo.vue'
 
 const userId = 'a1b2c3d4-e5f6-7890-1234-567890abcdef'
 
-let todoList = shallowRef();
+const todoList = shallowRef()
 
 onMounted(() => {
   fetch(`/api/v1/todos/${userId}`).then(async (response) => {
-    const todos = await response.json();
+    const todos = await response.json()
     todoList.value = todos
   })
 })
@@ -17,15 +17,25 @@ onMounted(() => {
 function addTodo() {
   console.log('addingTodo')
 }
-
 </script>
 
 <template>
-  <div class="bg-white w-full max-w-2xl rounded-xl border border-gray-100 hover:shadow-md px-2 py-4">
-    <h4 class="text-xl font-medium mb-2 pl-2">Task List</h4>
+  <div
+    class="
+      w-full max-w-2xl rounded-xl border border-gray-100 bg-white px-2 py-4
+      hover:shadow-md
+    "
+  >
+    <h4 class="mb-2 pl-2 text-xl font-medium">Task List</h4>
 
-    <button @click="addTodo()" class="w-full font-semibold text-blue-600 flex items-center transition-colors justify-start gap-4
-      rounded-full hover:bg-blue-50 p-2 hover:cursor-pointer mb-4">
+    <button
+      @click="addTodo()"
+      class="
+        mb-4 flex w-full items-center justify-start gap-4 rounded-full p-2 font-semibold
+        text-blue-600 transition-colors
+        hover:cursor-pointer hover:bg-blue-50
+      "
+    >
       <AddTask class="size-6 fill-blue-600" />
       <span>Add a task</span>
     </button>
