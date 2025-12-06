2ca2a7bce54e64500afe4f4bd741caafaaccd239
Benedikt Hofirek <benedikthofirek@gmail.com>
2025-12-06 14:36:38 +0100

    install material icons

--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted, shallowRef } from 'vue';
+import AddTask from '@material-symbols/svg-600/outlined/add_task.svg'
 import Todo from './Todo.vue';
 
 const userId = 'a1b2c3d4-e5f6-7890-1234-567890abcdef'
@@ -13,12 +14,23 @@ onMounted(() => {
   })
 })
 
+function addTodo() {
+  console.log('addingTodo')
+}
+
 </script>
 
 <template>
-  <div class="bg-white w-full max-w-2xl rounded-xl border border-gray-100 hover:shadow-md p-4">
-    <h4 class="text-xl font-medium mb-2">Task List</h4>
-    <ul v-if="todoList">
+  <div class="bg-white w-full max-w-2xl rounded-xl border border-gray-100 hover:shadow-md px-2 py-4">
+    <h4 class="text-xl font-medium mb-2 pl-2">Task List</h4>
+
+    <button @click="addTodo()" class="w-full font-semibold text-blue-600 flex items-center transition-colors justify-start gap-4
+      rounded-full hover:bg-blue-50 p-2 hover:cursor-pointer mb-4">
+      <AddTask class="size-6 fill-blue-600" />
+      <span>Add a task</span>
+    </button>
+
+    <ul v-if="todoList" class="px-2">
       <Todo v-for="todo in todoList" :key="todo.id" :todo />
     </ul>
     <div v-else>Fetching todo list...</div>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -12,6 +13,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    svgLoader(),
     tailwindcss(),
   ],
   resolve: {
